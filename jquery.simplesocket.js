ef3843855b467027bd6750106bb41308bbcbe4bb
Jannis Bloemendal <j.bloemendal@posteo.de>
2016-01-30 17:16:02 +0100

    initial setup - needs bug fixing, test, etc...

---
+++ jquery.simplesocket.js
@@ -0,0 +1,225 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+(function($){
+    var SimpleSocket = (function() {
+         var _opt;
+         var _ws;
+         var _reconnectTries;
+         var _reConnectDeferred = null;
+         var _listeners = [];
+
+         var _webSocket = function(opt) {
+             var ws;
+             if (opt.protocols) {
+                 ws = window['MozWebSocket'] ? new MozWebSocket(opt.url, opt.protocols) : window['WebSocket'] ? new WebSocket(opt.url, opt.protocols) : null;
+             } else {
+                 ws = window['MozWebSocket'] ? new MozWebSocket(opt.url) : window['WebSocket'] ? new WebSocket(opt.url) : null;
+             }
+
+             if (!ws) {
+                 return null;
+             }
+
+             $(ws).bind('open', opt.open)
+             .bind('close', opt.close)
+             .bind('message', function(e) {
+                var json = $.evalJSON(e.originalEvent.data);
+                if (opt[e.type]) {
+                    opt[e.type].call(this, json);
+                }
+             });
+             ws.onerror = function(e) { // cross-browser
+                 if (opt.error) {
+                     opt.error.call(this, e);
+                 }
+             };
+
+             return ws;
+         };
+
+         var _connect = function() {
+             console.log('connection attempt');
+             var attempt = jQuery.Deferred();
+             if (_ws) {
+                 if (_ws.readyState === 1) {
+                     console.log('connected');
+                     attempt.resolve(_ws);
+                     return attempt.promise();
+                 } else {
+                     // close previous socket
+                     _ws.close();
+                 }
+             }
+
+            _ws = _webSocket({
+                'url': _opt.url,
+                open: function(e) {
+                    console.log('socket open');
+                    var sock = this;
+                    // caution, only resolves when called 2nd time (ff)
+                    if (attempt) {
+                        window.setTimeout(function() {
+                            attempt.resolve(sock);
+                        }, 20);
+                    }
+                },
+                close: function(e) {
+                    console.log('close');
+                    for (var i=0, len=_listeners.length; i<len; i++) {
+                        _listeners[i].deferred.resolve();
+                    }
+
+                    if (attempt) {
+                        attempt.rejectWith(e);
+                    }
+                },
+                message: function(message) {
+                    for (var i=0, len=_listeners.length; i<len; i++) {
+                        try {
+                            console.log(message);
+                            _listeners[i].deferred.notify(message);
+                        } catch (error) {
+                            _listeners[i].deferred.reject(error);
+                        }
+                    }
+                },
+                error: function(e) {
+                    if (attempt) {
+                        attempt.rejectWith(e);
+                    }
+                }
+            });
+
+             return attempt.promise();
+         };
+
+         var _isConnected = function() {
+             return _ws !== null && _ws.readyState === 1;
+         }
+
+         var _reConnect = function() {
+             console.log('reconnect');
+             if (!_reConnectDeferred || _reConnectDeferred.state() !== 'pending') {
+                 _reConnectTries = 60; // 10min
+                 _reConnectDeferred = jQuery.Deferred();
+             }
+
+             if (_ws.readyState === 1) {
+                 console.log('reconnected');
+                 _reConnectDeferred.resolve(_ws);
+             } else {
+                 _connect().done(function() {
+                     console.log('reconnected');
+                    _reConnectDeferred.resolve(_ws);
+                 }).fail(function() {
+                    console.log('reconnect retry: '+_reConnectTries);
+                    if (_reConnectTries-- > 0) {
+                       window.setTimeout(function() {
+                           _reConnect();
+                       }, 10000);
+                    } else {
+                       console.log('reconnect failed');
+                       _reConnectDeferred.rejectWith();
+                    }
+                 });
+             }
+
+             return _reConnectDeferred.promise();
+         }
+
+         var _send = function(data) {
+             var attempt = jQuery.Deferred();
+             if (!_isConnected()) {
+                 (function(json) {
+                     _reConnect().done(function(ws) {
+                         ws.send(json);
+                         attempt.resolve();
+                     }).fail(function() {
+                         attempt.rejectWith();
+                     });
+                 })(JSON.stringify(data));
+             } else {
+                 _ws.send(JSON.stringify(data));
+                 attempt.resolve();
+             }
+             return attempt.promise();
+         };
+
+         var _indexOfListener = function(listener) {
+            for (var i=0, len=_listeners.length; i<len; i++) {
+                if (_listeners[i].listener === listener) {
+                    return i;
+                }
+            }
+            return -1;
+         };
+
+         var api = {
+
+             init: function(opt) {
+                _opt = opt;
+             },
+
+             connect: function() {
+               return _connect();
+             },
+
+             reConnect: function() {
+                return _reConnect();
+             },
+
+             isConnected: function() {
+                return _isConnected();
+             },
+
+             send: function(data) {
+                return _send(data);
+             },
+
+             listen: function(listener) {
+                var d = jQuery.Deferred();
+                if (_indexOfListener(listener) !== -1) {
+                    d.reject(new Error('Listener already listening.'));
+                } else {
+                    var d = jQuery.Deferred();
+                    d.notify(function() {
+                        listener.apply(this, arguments);
+                    });
+                    _listeners.push({ 'deferred': d, 'listener': listener });
+                }
+                return d.promise();
+             },
+
+             remove: function(listener) {
+                var index = _indexOfListener(listener);
+                if (index !== -1) {
+                    _listeners.splice(index, 1);
+                }
+             }
+         };
+         return api;
+     })();
+
+    $.extend({
+        simpleSocket: function(opt) {
+            SimpleSocket.init(opt);
+            return SimpleSocket;
+        }
+    });
+})(jQuery);
---
+++ tests/server.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets server
+    // we don't have to implement anything.
+});
+server.listen(3000, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        console.log(message);
+        connection.send(JSON.stringify(message));
+    });
+
+    connection.on('close', function(connection) {
+        // close user connection
+    });
+});
